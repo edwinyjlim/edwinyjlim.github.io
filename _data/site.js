5b2cba861545199c11281f7fde780ced8a51831a
Edwin Lim <edwinyjlim@gmail.com>
2022-07-18 13:12:40 -0400

    disable dark mode by default

--- _data/site.js
+++ _data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
   meta: {
-    title: "EYL's Portfolio",
+    title: "EYL - Portfolio",
     description: "This is my technical writing portfolio.",
     lang: "en",
     siteUrl: "https://example.com/",
